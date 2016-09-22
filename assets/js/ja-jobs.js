3dc41a3caf4fd52afde0ad3f22af65079df97994
Jacob Burenstam <burenstam@gmail.com>
2016-09-22 18:11:58 +0200

    Enhance job-cards

--- assets/js/ja-jobs.js
+++ assets/js/ja-jobs.js
@@ -1,10 +1,17 @@
 (function(window) {
+  var numberOfJobs = 3;
+  var path = '/jobs?include=company,category,hourly-pay'
+  var pageParam = '&page' + encodeURIComponent('[') + 'size' + encodeURIComponent(']') + '=' + numberOfJobs;
+  // var filterParam = '&filter' + encodeURIComponent('[') + 'hidden' + encodeURIComponent(']') + '=true';
+  var filterParam = '';
+  var sortParam = 'sort=filled';
+
   JA_API_URL = 'https://api.justarrived.se/api/v1';
-  JOBS_ENDPOINT = JA_API_URL + '/jobs?include=company&page' + encodeURIComponent('[') + 'size' + encodeURIComponent(']') + '=3';
+  JOBS_ENDPOINT = JA_API_URL + path + pageParam + filterParam + sortParam;
 
   function getJobs(callback) {
     $.getJSON(JOBS_ENDPOINT, function(response) {
-      callback(response.data);
+      callback(response.data, response.included);
     });
   }
 
@@ -13,30 +20,76 @@
     return template.replace(regex, value);
   }
 
+  function getCompanyName(includedData, id) {
+    var name;
+    includedData.forEach(function(resource) {
+      if (resource.type === 'companies' && resource.id === id) {
+        name = resource.attributes.name;
+      }
+    });
+    return name;
+  }
+
+  function getCategoryName(includedData, id) {
+    var name;
+    includedData.forEach(function(resource) {
+      if (resource.type === 'categories' && resource.id === id) {
+        name = resource.attributes.name;
+      }
+    });
+    return name;
+  }
+
+  function getHourlyPayValue(includedData, id) {
+    var value;
+    includedData.forEach(function(resource) {
+      if (resource.type === 'hourly-pays' && resource.id === id) {
+        value = resource.attributes['rate-excluding-vat'];
+      }
+    });
+    return value;
+  }
+
+  function formatJobTemplate(template, jobData, includedData) {
+    var innerHTML = '';
+    var jobAtrs = jobData.attributes;
+
+    var companyId = jobData.relationships.company.data.id;
+    var categoryId = jobData.relationships.category.data.id;
+    var hourlyPayId = jobData.relationships['hourly-pay'].data.id;
+
+    var company = getCompanyName(includedData, companyId);
+    var category = getCategoryName(includedData, categoryId);
+    var hourlyPay = getHourlyPayValue(includedData, hourlyPayId);
+
+    var hours = jobAtrs.hours;
+    var amount = hours * hourlyPay;
+    var maxDescriptionLength = 30;
+    var description = (jobAtrs['short-description'] || jobAtrs['description']).substring(0, maxDescriptionLength);
+
+    innerHTML = formatTemplate(template, '%job_company%', company);
+    innerHTML = formatTemplate(innerHTML, '%job_category%', category);
+    innerHTML = formatTemplate(innerHTML, '%job_amount%', amount);
+    innerHTML = formatTemplate(innerHTML, '%job_hours%', hours);
+    innerHTML = formatTemplate(innerHTML, '%job_hourly_pay%', hourlyPay);
+    return formatTemplate(innerHTML, '%job_description%', description);
+  }
+
   function createCards(targetSelector, templateSelector) {
     var $target = $(targetSelector);
     var $template = $(templateSelector);
+
+    if ($target.length === 0 || $template.length === 0) {
+      return;
+    }
+
     var template = $template.html().toString();
     var html = '';
 
-    getJobs(function(jobsData) {
+    getJobs(function(jobsData, includedData) {
       jobsData.forEach(function(jobData) {
-        var innerHTML = '';
-        var jobAtrs = jobData.attributes;
-
-        var company = 'Universum';
-        var category = 'Containerstädare';
-        var amount = '830';
-        var hourlyPay= 149;
-        var hours = jobAtrs.hours;
-        var description = (jobAtrs['short-description'] || jobAtrs['description']).substring(0, 50);
-
-        innerHTML = formatTemplate(template, '%job_company%', company);
-        innerHTML = formatTemplate(innerHTML, '%job_category%', category);
-        innerHTML = formatTemplate(innerHTML, '%job_amount%', amount);
-        innerHTML = formatTemplate(innerHTML, '%job_hours%', hours);
-        innerHTML = formatTemplate(innerHTML, '%job_hourly_pay%', hourlyPay);
-        html += formatTemplate(innerHTML, '%job_description%', description);
+        var jobId = jobData.id;
+        html += formatJobTemplate(template, jobData, includedData, jobId);
       });
 
       $target.append(html);
--- assets/js/site.js
+++ assets/js/site.js
@@ -1,11 +1,22 @@
 (function(window) {
+  function initHourlyPay() {
+    // Create hourly pay input list (used for job registrations)
+    window.HourlyPaysInput.createInputList({
+      targetSelector: '#js-hourly-salary-input-target',
+      inputTemplate: '#js-hourly-salary-input-template'
+    });
+  }
+
+  function initJobCards() {
+    var template = '#js-job-cards-template';
+    var target = '#js-job-cards';
+    window.JAJobs.createCards(target, template);
+  }
+
   var Site = {
     init: function() {
-      // Create hourly pay input list (used for job registrations)
-      window.HourlyPaysInput.createInputList({
-        targetSelector: '#js-hourly-salary-input-target',
-        inputTemplate: '#js-hourly-salary-input-template'
-      });
+      initHourlyPay();
+      initJobCards();
     }
   }
 
