4cedd142194c3e6a34abf1666ab24b8aa6f1a13a
Jacob Burenstam <burenstam@gmail.com>
2016-09-26 12:02:41 +0200

    Add 50/50 module

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -87,23 +87,6 @@ var timeToWaitForLast = 100;
  *
 */
 
-/*
- * We're going to swap out the gravatars.
- * In the functions.php file, you can see we're not loading the gravatar
- * images on mobile to save bandwidth. Once we hit an acceptable viewport
- * then we can swap out those images since they are located in a data attribute.
-*/
-function loadGravatars() {
-  // set the viewport using the function above
-  viewport = updateViewportDimensions();
-  // if the viewport is tablet or larger, we load in the gravatars
-  if (viewport.width >= 768) {
-  jQuery('.comment img[data-gravatar]').each(function(){
-    jQuery(this).attr('src',jQuery(this).attr('data-gravatar'));
-  });
-	}
-} // end function
-
 
 // Equal heights
 /**
@@ -506,6 +489,7 @@ function loadGravatars() {
             'The username portion of the email address is invalid (the portion before the @: )' : 4,
             'This email address looks fake or invalid. Please enter a real email address'       : 5
         },
+				// TODO: Provide translations
         translations: {
             'en': null
         },
