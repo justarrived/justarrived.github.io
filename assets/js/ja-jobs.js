ce3f74a40fbc50df7d7bc1a82a05b931c9473cdb
Tobias Tikka <tobias.tikka@gmail.com>
2016-09-28 18:24:02 +0200

    Add job name to display in page

--- assets/js/ja-jobs.js
+++ assets/js/ja-jobs.js
@@ -69,6 +69,7 @@
     innerHTML = formatTemplate(template, '%job_company%', company.name);
     innerHTML = formatTemplate(innerHTML, '%job_city%', company.city);
     innerHTML = formatTemplate(innerHTML, '%job_category%', category.name);
+    innerHTML = formatTemplate(innerHTML, '%job_name%', jobAtrs.name);
     innerHTML = formatTemplate(innerHTML, '%job_amount%', amount);
     innerHTML = formatTemplate(innerHTML, '%job_hours%', hours);
     innerHTML = formatTemplate(innerHTML, '%job_hourly_pay%', grossSalary);
