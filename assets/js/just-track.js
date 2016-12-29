1c697be429fc7e70c6b57cd26dd09eab8ab9922c
Jacob Burenstam <burenstam@gmail.com>
2016-12-29 20:39:02 +0100

    compile

--- assets/js/just-track.js
+++ assets/js/just-track.js
@@ -77,6 +77,15 @@
     });
   }
 
+  function trackCookieAcceptClicks() {
+    $(document).on('click', '.js-cookie-accept-btn', function() {
+      gaTrack(
+        'cookie',
+        'click',
+        'Cookie accept');
+    });
+  }
+
   function initTrackOfDOMElements() {
     $('[data-action]').each(function() {
       var $element = $(this);
@@ -109,6 +118,7 @@
 
   var JustTrack = {
     init: function() {
+      trackCookieAcceptClicks();
       trackCompanySignup();
       trackNewcomerSignupStart();
       trackNewcomerSignupDone();
