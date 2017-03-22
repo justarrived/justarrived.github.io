90b0a38eee416112f01330c1347f52e38303a909
Sebastian Bellevik <bellevik@gmail.com>
2017-03-22 18:49:00 +0100

    Carousel is done

--- assets/js/company-scripts.js
+++ assets/js/company-scripts.js
@@ -1,33 +1,52 @@
+---
+---
+/*In order to access variables from _config.yml we are allowing this here,
+* this so we only specify the categories in one location.
+*/
+
 jQuery(document).ready(function($) {
+  var currentIndex = 0
+  var rotateCarousel = true;
+
   var people = [{
-                  'title': 'First: Sara Brandt, Västbyhem',
+                  'title': 'Sara Brandt, Västbyhem',
                   'text': 'First: För oss var det otroligt bra att anställa visa Just Arrived. Vi kontaktade Just Arrived på torsdagen och redan på måndagen hade vi en mycket duktig person som började på vårt kontor. Alla på kontoret tyckte det var fantastiskt bra att vi kunde hjälpa någon att komma in i sammhället.',
-                  'img': '../images/profile-pictures/sara-brandt.jpg'
+                  'img': 'sara-brandt.jpg'
                 },
                 {
-                  'title': 'Second: Sara Brandt, Västbyhem',
+                  'title': 'Johnny Warström, VD, Mentimeter',
                   'text': 'Second: För oss var det otroligt bra att anställa visa Just Arrived. Vi kontaktade Just Arrived på torsdagen och redan på måndagen hade vi en mycket duktig person som började på vårt kontor. Alla på kontoret tyckte det var fantastiskt bra att vi kunde hjälpa någon att komma in i sammhället.',
-                  'img': '../images/profile-pictures/johnny-warstrom.jpg'
+                  'img': 'johnny-warstrom.jpg'
                 },
                 {
-                  'title': 'Third: Sara Brandt, Västbyhem',
+                  'title': 'Fredrik Skytt, VD, Utbudet',
                   'text': 'Third: För oss var det otroligt bra att anställa visa Just Arrived. Vi kontaktade Just Arrived på torsdagen och redan på måndagen hade vi en mycket duktig person som började på vårt kontor. Alla på kontoret tyckte det var fantastiskt bra att vi kunde hjälpa någon att komma in i sammhället.',
-                  'img': '../images/profile-pictures/fredrik-skytt.png'
+                  'img': 'fredrik-skytt.png'
                 }]
 
-  //changeInfo(0)
+  changeInfo(currentIndex);
+
+  setInterval(function(){
+    currentIndex = (currentIndex + 1) % people.length
+    if(rotateCarousel) {
+      changeInfo(currentIndex)
+    }
+  }, 10000);
 
   $('.company-recomendation-name-cell').on('click', function(event) {
-    $('.company-recomendation-name-cell').removeClass('company-recomendation-name-cell-active');
-    $(this).addClass('company-recomendation-name-cell-active');
-
     var index = $.inArray(this, $('.company-recomendation-name-cell'));
+    currentIndex = index
+    rotateCarousel = false; // The carousel should stop rotating if the User makes a selection
     changeInfo(index)
   });
 
+  // Change the info currently being displayed
   function changeInfo(index) {
-    //$('.company-recomendation-name-cell').removeClass('company-recomendation-name-cell-active');
-    //$('.company-recomendation-name-cell')[index].addClass('company-recomendation-name-cell-active'); // Doesn't work
+    $('.company-recomendation-name-cell').removeClass('company-recomendation-name-cell-active');
+    $('#p' + index).addClass('company-recomendation-name-cell-active');
+
+    $('#profile-image').css('background-image', 'url(../assets/images/profile-pictures/' + people[index].img + ')')
+
     $('#company-recommendation-current-text').text(people[index].text)
     $('#company-recommendation-current-name').text(people[index].title)
   }
