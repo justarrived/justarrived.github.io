6d86275003181e852dc275191849db28e76053d1
Jacob Burenstam <burenstam@gmail.com>
2017-04-11 15:40:30 +0200

    compile

---
+++ assets/js/company-scripts.js
@@ -0,0 +1,56 @@
+/*In order to access variables from _config.yml we are allowing this here,
+* this so we only specify the categories in one location.
+*/
+
+jQuery(document).ready(function($) {
+  var currentIndex = 0
+  var rotateCarousel = true;
+
+  var people = [{
+                  'title': 'Sara Brandt, Västbyhem',
+                  'text': 'Just Arrived tar bort alla trösklar för att jobba med nyanlända.',
+                  'img': 'sara-brandt.jpg'
+                },
+                {
+                  'title': 'Johnny Warström, VD, Mentimeter',
+                  'text': 'Att anställa via Just Arrived visade sig vara otroligt enkelt och snabbt. Personen som utförde uppdraget var både artig, nyfiken och öppen. Att få sitta tillsammans och njuta av mat från Aleppo och träffa en nysvensk fick effekten att vi reflekterade om vår omvärld och fick en varm stund tillsammans.',
+                  'img': 'johnny-warstrom.jpg'
+                },
+                {
+                  'title': 'Fredrik Skytt, VD, Utbudet',
+                  'text': 'För oss var det otroligt bra att anställa via Just Arrived. Vi kontaktade Just Arrived på torsdagen och redan på måndagen hade vi en mycket duktig person som började på vårt kontor. Alla på kontoret tyckte det var fantastiskt bra att vi kunde hjälpa någon att komma in i samhället.',
+                  'img': 'fredrik-skytt.png'
+                }]
+
+  changeInfo(currentIndex);
+
+  setInterval(function(){
+    currentIndex = (currentIndex + 1) % people.length
+    if(rotateCarousel) {
+      changeInfo(currentIndex)
+    }
+  }, 10000);
+
+  $('.company-recomendation-name-cell').on('click', function(event) {
+    var index = $.inArray(this, $('.company-recomendation-name-cell'));
+    currentIndex = index
+    rotateCarousel = false; // The carousel should stop rotating if the User makes a selection
+    changeInfo(index)
+  });
+
+  // Change the info currently being displayed
+  function changeInfo(index) {
+    $('.company-recomendation-name-cell').removeClass('company-recomendation-name-cell-active');
+    $('#p' + index).addClass('company-recomendation-name-cell-active');
+
+    $('#profile-image').css('background-image', 'url(/assets/images/profile-pictures/' + people[index].img + ')')
+    $('#profile-image').toggleClass('profile-circle-animation');
+
+    setTimeout(function(){
+      $('#profile-image').toggleClass('profile-circle-animation');
+    }, 1500);
+
+    $('#company-recommendation-current-text').text(people[index].text)
+
+  }
+});
